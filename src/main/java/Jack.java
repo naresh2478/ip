9ebbbfe055dd8e60c973d075b50f38b9eaf85573
Naresh <naresh30.nehru@gmail.com>
2025-08-20 18:04:21 +0800

    Level-0

---
+++ src/main/java/Jack.java
@@ -0,0 +1,17 @@
+public class Jack {
+    public static void main(String[] args) {
+        // String logo = " ____        _        \n"
+        //         + "|  _ \\ _   _| | _____ \n"
+        //         + "| | | | | | | |/ / _ \\\n"
+        //         + "| |_| | |_| |   <  __/\n"
+        //         + "|____/ \\__,_|_|\\_\\___|\n";
+        // System.out.println("Hello from\n" + logo);
+
+        // Greet user
+        System.out.println("Hello! I'm Jack");
+        System.out.println("What can I do for you?");
+        
+        // Exit message
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+}
