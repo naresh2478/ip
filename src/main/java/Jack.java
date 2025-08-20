1fcdbf768ab07f110d3e455ce49481f6f6e3c414
Naresh <naresh30.nehru@gmail.com>
2025-08-20 18:35:47 +0800

    Level-2

--- src/main/java/Jack.java
+++ src/main/java/Jack.java
@@ -5,6 +5,10 @@ public class Jack {
         
         Scanner scanner = new Scanner(System.in);
 
+        // Array to store tasks (no more than 100 tasks)
+        String[] tasks = new String[100];
+        int taskCount = 0;  // Keeps track of the number of tasks entered
+
         // Greet user
         System.out.println("Hello! I'm Jack");
         System.out.println("What can I do for you?");
@@ -19,8 +23,19 @@ public class Jack {
                 break;
             }
 
-            // Echo the user's input
-            System.out.println(userInput);
+            // If the user types "list", display all stored tasks
+            else if (userInput.equals("list")) {
+                for (int i = 0; i < taskCount; i++) {
+                    System.out.println((i + 1) + ". " + tasks[i]);
+                }
+            }
+
+             // If the user types anything else, add it as a task
+            else {
+                tasks[taskCount] = userInput;  // Store the task
+                taskCount++;  // Increment task count
+                System.out.println("added: " + userInput);
+            }
         }
         
         // Close the scanner
