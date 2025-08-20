4aa527b502c28d1b5ea65e67776085dc796cf927
Naresh <naresh30.nehru@gmail.com>
2025-08-20 18:27:29 +0800

    Level-1

--- src/main/java/Jack.java
+++ src/main/java/Jack.java
@@ -1,17 +1,29 @@
+import java.util.Scanner;
+
 public class Jack {
     public static void main(String[] args) {
-        // String logo = " ____        _        \n"
-        //         + "|  _ \\ _   _| | _____ \n"
-        //         + "| | | | | | | |/ / _ \\\n"
-        //         + "| |_| | |_| |   <  __/\n"
-        //         + "|____/ \\__,_|_|\\_\\___|\n";
-        // System.out.println("Hello from\n" + logo);
+        
+        Scanner scanner = new Scanner(System.in);
 
         // Greet user
         System.out.println("Hello! I'm Jack");
         System.out.println("What can I do for you?");
+
+        while (true) {
+            // Read the user input
+            String userInput = scanner.nextLine();
+
+            // If the user types "bye", exit the program
+            if (userInput.equals("bye")) {
+                System.out.println("Bye. Hope to see you again soon!");
+                break;
+            }
+
+            // Echo the user's input
+            System.out.println(userInput);
+        }
         
-        // Exit message
-        System.out.println("Bye. Hope to see you again soon!");
+        // Close the scanner
+        scanner.close();
     }
 }
