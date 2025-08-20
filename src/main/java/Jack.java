0a2f6775daf61ce77a0d6a68e7e0caa9c24d38c4
Naresh <naresh30.nehru@gmail.com>
2025-08-20 20:29:36 +0800

    Level-3

--- src/main/java/Jack.java
+++ src/main/java/Jack.java
@@ -6,7 +6,7 @@ public class Jack {
         Scanner scanner = new Scanner(System.in);
 
         // Array to store tasks (no more than 100 tasks)
-        String[] tasks = new String[100];
+        Task[] tasks = new Task[100];
         int taskCount = 0;  // Keeps track of the number of tasks entered
 
         // Greet user
@@ -25,14 +25,30 @@ public class Jack {
 
             // If the user types "list", display all stored tasks
             else if (userInput.equals("list")) {
+                System.out.println("Here are the tasks in your list:");
                 for (int i = 0; i < taskCount; i++) {
-                    System.out.println((i + 1) + ". " + tasks[i]);
+                    System.out.println((i + 1) + ". " + tasks[i].getStatusIcon()  + " " + tasks[i].getDescription());
                 }
+            }
+
+            // If the user types "mark X", mark the task as done
+            else if (userInput.startsWith("mark")) {
+                int taskNumber = Integer.parseInt(userInput.split(" ")[1]) - 1; // Get the task index (0-based)
+                tasks[taskNumber].markAsDone();
+                System.out.println("Nice! I've marked this task as done:\n" + tasks[taskNumber].getStatusIcon() + " " + tasks[taskNumber].getDescription());
+                
+            }
+
+            // If the user types "unmark X", unmark the task
+            else if (userInput.startsWith("unmark")) {
+                int taskNumber = Integer.parseInt(userInput.split(" ")[1]) - 1; // Get the task index (0-based)
+                tasks[taskNumber].unmark();
+                System.out.println("OK, I've marked this task as not done yet:\n" + tasks[taskNumber].getStatusIcon() + " " + tasks[taskNumber].getDescription());
             }
 
              // If the user types anything else, add it as a task
             else {
-                tasks[taskCount] = userInput;  // Store the task
+                tasks[taskCount] = new Task(userInput);  // Store the task
                 taskCount++;  // Increment task count
                 System.out.println("added: " + userInput);
             }
@@ -42,3 +58,5 @@ public class Jack {
         scanner.close();
     }
 }
+
+
---
+++ src/main/java/Task.java
@@ -0,0 +1,28 @@
+public class Task {
+    protected String description;
+    protected boolean isDone;
+
+    public Task(String description) {
+        this.description = description;
+        this.isDone = false;
+    }
+
+    public String getStatusIcon() {
+        return (isDone ? "[X]" : "[ ]"); // mark done task with X
+    }
+
+    //method to mark the task as done
+    public void markAsDone() {
+        this.isDone = true;
+    }
+
+    // Method to mark the task as not done
+    public void unmark() {
+        this.isDone = false;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+}
