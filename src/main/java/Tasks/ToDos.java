62de57235cf37f9845f87abed8b7db1e321e729a
Naresh <naresh30.nehru@gmail.com>
2025-09-03 17:49:52 +0800

    Add Tests

--- src/main/java/Tasks/ToDos.java
+++ src/main/java/Tasks/ToDos.java
@@ -1,11 +1,26 @@
 package Tasks;
 
+import java.util.Objects;
+
 public class ToDos extends Task {
 
     protected String description;
 
     public ToDos(String description) {
         super(description);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ToDos toDos = (ToDos) o;
+        return Objects.equals(description, toDos.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(description);
     }
 
     @Override
