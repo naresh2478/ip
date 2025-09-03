0c17e173e13ccfc04ce79460697169693f77bcd6
Naresh <naresh30.nehru@gmail.com>
2025-09-03 17:51:53 +0800

    Tests Modified

--- src/main/java/Tasks/ToDos.java
+++ src/main/java/Tasks/ToDos.java
@@ -11,19 +11,6 @@ public class ToDos extends Task {
     }
 
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        ToDos toDos = (ToDos) o;
-        return Objects.equals(description, toDos.description);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(description);
-    }
-
-    @Override
     public String toString() {
         return "[T]" + super.toString();
     }
