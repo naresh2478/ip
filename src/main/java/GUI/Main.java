04ed753245d517d622dc7062cb353d479263f0b2
Naresh <naresh30.nehru@gmail.com>
2025-09-20 16:19:44 +0800

    Improve code quality

--- src/main/java/GUI/Main.java
+++ src/main/java/GUI/Main.java
@@ -24,7 +24,7 @@ public class Main extends Application {
             AnchorPane ap = fxmlLoader.load();
             Scene scene = new Scene(ap);
             stage.setScene(scene);
-            fxmlLoader.<MainWindow>getController().setJack(jack);  // inject the Duke instance
+            fxmlLoader.<MainWindow>getController().setJack(jack);
             stage.show();
         } catch (IOException e) {
             e.printStackTrace();
